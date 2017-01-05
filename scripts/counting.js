4290b08898f0795eb2e9b0de0ee83bd227409d77
Motoki Kobayashi <motoki.kobayashi@studio-umi.jp>
2017-01-05 11:04:36 +0900

    cron実行時に通知チャネルを選択可能に変更

--- scripts/counting.js
+++ scripts/counting.js
@@ -2,8 +2,20 @@ const co = require('co');
 const HatenaCounting = require('../lib/HatenaCounting');
 
 const CountLists = [
-  {name: 'SUPER GT 2017 Rd.6 SUZUKA', url: 'http://counting.hatelabo.jp/count/u7pi5kfr92'},
+  {name: 'SUPER GT 2017 Rd.6 SUZUKA', url: 'http://counting.hatelabo.jp/count/u7pi5kfr92', channel: 'supergt2017'},
 ];
+
+/**
+ *
+ */
+const setAttachments = (countInfo) => {
+  return {
+    fallback: countInfo.counter_name,
+    title: countInfo.counter_name,
+    title_link: countInfo.counter_url,
+    image_url: countInfo.image,
+  };
+};
 
 /**
  * BOTスクリプト本体.
@@ -12,34 +24,24 @@ const CountLists = [
 module.exports = (bot) => {
   // BOTに対し `記念日` とメッセージを送信すると路線リストの遅延情報を返す.
   bot.respond(/記念日/, (msg) => {
-    for (let list of CountLists) {
-      co(function*() {
+    co(function*() {
+      for (let list of CountLists) {
         const countInfo = yield HatenaCounting.getCounterInfo(list.url);
-        const attachments = {
-          fallback: countInfo.counter_name,
-          title: countInfo.counter_name,
-          title_link: countInfo.counter_url,
-          image_url: countInfo.image,
-        };
-
+        const attachments = setAttachments(countInfo);
         msg.send({attachments: [attachments]});
-      });
-    }
+      }
+    });
   });
 
   // 毎朝イベント日を通知する処理.
   bot.jobs.add('0 0 9 * * *', () => {
-    for (let list of CountLists) {
-      co(function*() {
+    co(function*() {
+      for (let list of CountLists) {
         const countInfo = yield HatenaCounting.getCounterInfo(list.url);
-        const attachments = {
-          fallback: countInfo.counter_name,
-          title: countInfo.counter_name,
-          title_link: countInfo.counter_url,
-          image_url: countInfo.image,
-        };
-        bot.send({attachments: [attachments]}, 'supergt2017');
-      });
-    }
+        const attachments = setAttachments(countInfo);
+        const channel = list.channel || bot.default_channel;
+        bot.send({attachments: [attachments]}, channel);
+      }
+    });
   });
 };
