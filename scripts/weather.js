8a7f177c2cefa0e82ce52d8f62c2b569158ff3ad
Motoki Kobayashi <motoki.kobayashi@studio-umi.jp>
2017-01-09 13:44:04 +0900

    天気の条件に地名と区分を選べるように変更

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,7 +9,14 @@ const WeatherLists = [
 /**
  *
  */
-const setAttachments = (weatherInfo, url) => {
+const setAttachments = (weatherInfo, url, pattern) => {
+  if (pattern === 'weekly') {
+    return {
+      fallback: '週の天気',
+      text: '週の天気',
+      image_url: weatherInfo.image,
+    };
+  }
   return {
     fallback: '天気予報[' + weatherInfo.area + ']：' + weatherInfo.weather,
     title: '[' + weatherInfo.area + '] ' + weatherInfo.date + 'の天気',
@@ -24,12 +31,31 @@ const setAttachments = (weatherInfo, url) => {
  * @param {object} bot shrikeボット.
  */
 module.exports = (bot) => {
-  // BOTに対し `天気` とメッセージを送信すると路線リストの遅延情報を返す.
-  bot.respond(/天気/, (msg) => {
-    for (let weatherList of WeatherLists) {
+  // BOTに対し `天気 地名 取得対象` とメッセージを送信すると路線リストの遅延情報を返す.
+  bot.respond(/^(?:天気|weather) (.\S+)\s?(今日|today|明日|tomorrow|週|week|weekly)?/i, (msg) => {
+    const area = msg.match[1];
+    let pattern = msg.match[2] || 'today';
+
+    const filterWeatherList = WeatherLists.filter((item) => {
+      return (item.name == area);
+    });
+
+    if (filterWeatherList.length === 0) {
+      return msg.send('[' + area + '] の天気予報取得URLは未登録です。');
+    }
+
+    if (pattern === '今日') {
+      pattern = 'today';
+    } else if (pattern === '明日') {
+      pattern = 'tomorrow';
+    } else if (pattern === '週' || pattern === 'week') {
+      pattern = 'weekly';
+    }
+
+    for (let weatherList of filterWeatherList) {
       co(function*() {
-        const weatherInfo = yield YahooWeather.getWeather(weatherList.url, 'today');
-        const attachments = setAttachments(weatherInfo, weatherList.url);
+        const weatherInfo = yield YahooWeather.getWeather(weatherList.url, pattern);
+        const attachments = setAttachments(weatherInfo, weatherList.url, pattern);
         msg.send({attachments: [attachments]});
       });
     }
@@ -40,7 +66,7 @@ module.exports = (bot) => {
     for (let weatherList of WeatherLists) {
       co(function*() {
         const weatherInfo = yield YahooWeather.getWeather(weatherList.url, 'today');
-        const attachments = setAttachments(weatherInfo, weatherList.url);
+        const attachments = setAttachments(weatherInfo, weatherList.url, 'today');
         const channel = weatherList.channel || bot.default_channel.name;
         bot.send({attachments: [attachments]}, channel);
       });
